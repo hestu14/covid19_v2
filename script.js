7086ff1aec6fed0e089f2d70f5f1dfffdae2bc13
Hestu P <61925354+hestu14@users.noreply.github.com>
2020-05-20 00:05:18 +0700

    Ubah server data ver 1.1

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-$.ajax({url: "https://api.kawalcorona.com/indonesia/", success: function(result){
-    var positif =  result[0].positif;
-    var positif2 = positif.split(",");
-    var positif3 = positif2.join("");
+$.ajax({url: "https://kawalcovid19.harippe.id/api/summary", success: function(result){
+    var positif =  result.confirmed.value;
+    // var positif2 = positif.split(",");
+    // var positif3 = positif2.join("");
 
-    var sembuh = result[0].sembuh;
-    var sembuh2 = sembuh.split(",");
-    var sembuh3 = sembuh2.join("");
+    var sembuh = result.recovered.value;
+    // var sembuh2 = sembuh.split(",");
+    // var sembuh3 = sembuh2.join("");
 
-    var meninggal = result[0].meninggal;
-    var meninggal2 = meninggal.split(",");
-    var meninggal3 = meninggal2.join("");
+    var meninggal = result.deaths.value;
+    // var meninggal2 = meninggal.split(",");
+    // var meninggal3 = meninggal2.join("");
 
-    var perawatan = positif3 - sembuh3 - meninggal;
+    var perawatan = result.activeCare.value;
 
-    $("#terjangkit").html("Positif: " +positif3 + " orang");
-    $("#sembuh").html("Sembuh: " +sembuh3 + " orang");
-    $("#meninggal").html("Meninggal: " +meninggal3 + " orang");
+    $("#terjangkit").html("Positif: " +positif + " orang");
+    $("#sembuh").html("Sembuh: " +sembuh + " orang");
+    $("#meninggal").html("Meninggal: " +meninggal + " orang");
     $("#perawatan").html("Perawatan: " +perawatan + " orang");
 
 
@@ -43,56 +43,67 @@ $.ajax({url: "https://api.kawalcorona.com/indonesia/", success: function(result)
 });
 
 
-$(document).ready(function() {
-    $.ajax({url: "https://api.kawalcorona.com/indonesia/provinsi/", success: function(result){
-    
-        $(".row-2").html(
-            result[0].attributes.Provinsi+ " : " + result[0].attributes.Kasus_Posi + " orang<br>" +
-            result[1].attributes.Provinsi+ " : " + result[1].attributes.Kasus_Posi + " orang<br>" +
-            result[2].attributes.Provinsi+ " : " + result[2].attributes.Kasus_Posi + " orang<br>" +
-            result[3].attributes.Provinsi+ " : " + result[3].attributes.Kasus_Posi + " orang<br>" +
-            result[4].attributes.Provinsi+ " : " + result[4].attributes.Kasus_Posi + " orang<br>" +
-            result[5].attributes.Provinsi+ " : " + result[5].attributes.Kasus_Posi + " orang<br>" +
-            result[6].attributes.Provinsi+ " : " + result[6].attributes.Kasus_Posi + " orang<br>" +
-            result[7].attributes.Provinsi+ " : " + result[7].attributes.Kasus_Posi + " orang<br>" +
-            result[8].attributes.Provinsi+ " : " + result[8].attributes.Kasus_Posi + " orang<br>" +
-            result[9].attributes.Provinsi+ " : " + result[9].attributes.Kasus_Posi + " orang<br>" +
-            result[10].attributes.Provinsi+ " : " + result[10].attributes.Kasus_Posi + " orang<br>" +
-            result[11].attributes.Provinsi+ " : " + result[11].attributes.Kasus_Posi + " orang<br>" +
-            result[12].attributes.Provinsi+ " : " + result[12].attributes.Kasus_Posi + " orang<br>" +
-            result[13].attributes.Provinsi+ " : " + result[13].attributes.Kasus_Posi + " orang<br>" +
-            result[14].attributes.Provinsi+ " : " + result[14].attributes.Kasus_Posi + " orang<br>" +
-            result[15].attributes.Provinsi+ " : " + result[15].attributes.Kasus_Posi + " orang<br>" +
-            result[16].attributes.Provinsi+ " : " + result[16].attributes.Kasus_Posi + " orang<br>" +
-            result[17].attributes.Provinsi+ " : " + result[17].attributes.Kasus_Posi + " orang<br>" +
-            result[18].attributes.Provinsi+ " : " + result[18].attributes.Kasus_Posi + " orang<br>" +
-            result[19].attributes.Provinsi+ " : " + result[19].attributes.Kasus_Posi + " orang<br>" +
-            result[20].attributes.Provinsi+ " : " + result[20].attributes.Kasus_Posi + " orang<br>" +
-            result[21].attributes.Provinsi+ " : " + result[21].attributes.Kasus_Posi + " orang<br>" +
-            result[22].attributes.Provinsi+ " : " + result[22].attributes.Kasus_Posi + " orang<br>" +
-            result[23].attributes.Provinsi+ " : " + result[23].attributes.Kasus_Posi + " orang<br>" +
-            result[24].attributes.Provinsi+ " : " + result[24].attributes.Kasus_Posi + " orang<br>" +
-            result[25].attributes.Provinsi+ " : " + result[25].attributes.Kasus_Posi + " orang<br>" +
-            result[26].attributes.Provinsi+ " : " + result[26].attributes.Kasus_Posi + " orang<br>" +
-            result[27].attributes.Provinsi+ " : " + result[27].attributes.Kasus_Posi + " orang<br>" +
-            result[28].attributes.Provinsi+ " : " + result[28].attributes.Kasus_Posi + " orang<br>" +
-            result[29].attributes.Provinsi+ " : " + result[29].attributes.Kasus_Posi + " orang<br>" +
-            result[30].attributes.Provinsi+ " : " + result[30].attributes.Kasus_Posi + " orang<br>" +
-            result[31].attributes.Provinsi+ " : " + result[31].attributes.Kasus_Posi + " orang<br>" +
-            result[32].attributes.Provinsi+ " : " + result[32].attributes.Kasus_Posi + " orang<br>" +
-            result[33].attributes.Provinsi+ " : " + result[33].attributes.Kasus_Posi + " orang<br>"
-        
-        
-        );
+const get_provinsi = document.getElementById('indonesia-provinsi');
+const tabelprovinsi = document.getElementById('row-2');
 
-    }});
+get_provinsi.addEventListener('click', generateProvinsi);
+
+generateProvinsi();
+
+async function generateProvinsi() {
+provinsiRes = await fetch('https://indonesia-covid-19.mathdro.id/api/provinsi', {
+		headers: {
+			'Accept': 'application/json'
+        }
     });
+        
+const result = await provinsiRes.json();
+// console.log(result);
+
+tabelprovinsi.innerHTML = 
+            result.data[0].provinsi+ " : " + result.data[0].kasusPosi + " orang<br>" +
+            result.data[1].provinsi+ " : " + result.data[1].kasusPosi + " orang<br>" +
+            result.data[2].provinsi+ " : " + result.data[2].kasusPosi + " orang<br>" +
+            result.data[3].provinsi+ " : " + result.data[3].kasusPosi + " orang<br>" +
+            result.data[4].provinsi+ " : " + result.data[4].kasusPosi + " orang<br>" +
+            result.data[5].provinsi+ " : " + result.data[5].kasusPosi + " orang<br>" +
+            result.data[6].provinsi+ " : " + result.data[6].kasusPosi + " orang<br>" +
+            result.data[7].provinsi+ " : " + result.data[7].kasusPosi + " orang<br>" +
+            result.data[8].provinsi+ " : " + result.data[8].kasusPosi + " orang<br>" +
+            result.data[9].provinsi+ " : " + result.data[9].kasusPosi + " orang<br>" +
+            result.data[10].provinsi+ " : " + result.data[10].kasusPosi + " orang<br>" +
+            result.data[11].provinsi+ " : " + result.data[11].kasusPosi + " orang<br>" +
+            result.data[12].provinsi+ " : " + result.data[12].kasusPosi + " orang<br>" +
+            result.data[13].provinsi+ " : " + result.data[13].kasusPosi + " orang<br>" +
+            result.data[14].provinsi+ " : " + result.data[14].kasusPosi + " orang<br>" +
+            result.data[15].provinsi+ " : " + result.data[15].kasusPosi + " orang<br>" +
+            result.data[16].provinsi+ " : " + result.data[16].kasusPosi + " orang<br>" +
+            result.data[17].provinsi+ " : " + result.data[17].kasusPosi + " orang<br>" +
+            result.data[18].provinsi+ " : " + result.data[18].kasusPosi + " orang<br>" +
+            result.data[19].provinsi+ " : " + result.data[19].kasusPosi + " orang<br>" +
+            result.data[20].provinsi+ " : " + result.data[20].kasusPosi + " orang<br>" +
+            result.data[21].provinsi+ " : " + result.data[21].kasusPosi + " orang<br>" +
+            result.data[22].provinsi+ " : " + result.data[22].kasusPosi + " orang<br>" +
+            result.data[23].provinsi+ " : " + result.data[23].kasusPosi + " orang<br>" +
+            result.data[24].provinsi+ " : " + result.data[24].kasusPosi + " orang<br>" +
+            result.data[25].provinsi+ " : " + result.data[25].kasusPosi + " orang<br>" +
+            result.data[26].provinsi+ " : " + result.data[26].kasusPosi + " orang<br>" +
+            result.data[27].provinsi+ " : " + result.data[27].kasusPosi + " orang<br>" +
+            result.data[28].provinsi+ " : " + result.data[28].kasusPosi + " orang<br>" +
+            result.data[29].provinsi+ " : " + result.data[29].kasusPosi + " orang<br>" +
+            result.data[30].provinsi+ " : " + result.data[30].kasusPosi + " orang<br>" +
+            result.data[31].provinsi+ " : " + result.data[31].kasusPosi + " orang<br>" +
+            result.data[32].provinsi+ " : " + result.data[32].kasusPosi + " orang<br>" +
+            result.data[33].provinsi+ " : " + result.data[33].kasusPosi + " orang<br>" +
+            result.data[34].provinsi+ " : " + result.data[34].kasusPosi + " orang<br>";
+        // )        
+}
 
 // $(document).ready(function() {
 // $.ajax({url: "https://api.kawalcorona.com/indonesia/provinsi/", success: function(result){
 
-//     var provinsi = result[0].attributes.Provinsi;
-//     var kasus_pos = result[0].attributes.Provinsi.Kasus_Posi;
+//     var provinsi = result[0].data.provinsi;
+//     var kasus_pos = result[0].data.provinsi.Kasus_Posi;
 
 //     $(".row2").html(provinsi + " : " + kasus_pos);
 // });
